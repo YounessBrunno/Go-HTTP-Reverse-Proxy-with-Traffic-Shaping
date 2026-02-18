285a345270b4a818dca6f1e08ab60f8842f97a3e
youness oukhadjou <brunnotools@gmail.com>
2026-02-18 11:44:32 +0000

    feat: Refactor main package structure and implement basic HTTP handlers

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,7 +1,19 @@
-package main
+package (
+	"fmt"
+	"net/http"
+)
 
 
 func main() {
+   http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+   })
 
+   http.HandleFunc("/slow", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintf(w, "Sleept for 3 seconds!")
+   })
+
+   http.ListenAndServe(":9000", nil)
 }
 
